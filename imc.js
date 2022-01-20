1b8801febd8158568204e72e0980c96ec0f9fdf6
CsBryan0 <97749500+CsBryan0@users.noreply.github.com>
2022-01-20 08:43:36 -0300

    Add files via upload

---
+++ imc.js
@@ -0,0 +1,48 @@
+const calcular = document.getElementById('calcular');
+
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !=='' && altura !== '' && peso !=='') {
+
+        const valorIMC = (peso / (altura * altura)).toFixed(2);
+        let clasificacao = ''
+        
+        if (valorIMC < 18.5){
+            clasificacao = 'abaixo do peso.';
+        }
+        else if (valorIMC < 25){
+            clasificacao = 'com peso ideal.';
+        }
+        else if (valorIMC < 30){
+            clasificacao = 'levemente acima do peso.';
+        } 
+        else if (valorIMC < 35){
+            clasificacao = 'com obesidade grau I.';
+        }
+        else if (valorIMC < 40){
+            clasificacao = 'com obesidade grau II'
+        }
+        else {
+            clasificacao = 'com obesidade mórbida. Cuidado'
+        }
+        
+        
+
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${clasificacao}`
+    }else {
+        resultado.textContent = 'Preencha todos os campos!!!'
+    }
+
+
+}
+
+calcular.addEventListener('click', imc)
+
+
+
+
